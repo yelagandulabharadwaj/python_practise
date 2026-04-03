a3a219a90fb331262433f8933852af29b8648d73
yelagandulabharadwaj <yelagandulabharadwaj@gmail.com>
2026-04-03 17:44:30 +0530

    added catalog code changes

---
+++ catalog_codewars.py
@@ -0,0 +1,171 @@
+'''
+
+You are given a small extract of a catalog:
+
+s = "<prod><name>drill</name><prx>99</prx><qty>5</qty></prod>
+
+<prod><name>hammer</name><prx>10</prx><qty>50</qty></prod>
+
+<prod><name>screwdriver</name><prx>5</prx><qty>51</qty></prod>
+
+<prod><name>table saw</name><prx>1099.99</prx><qty>5</qty></prod>
+
+<prod><name>saw</name><prx>9</prx><qty>10</qty></prod>
+
+...
+(prx stands for price, qty for quantity) and an article i.e "saw".
+
+The function catalog(s, "saw") returns the line(s) corresponding to the article with $ before the prices:
+
+"table saw > prx: $1099.99 qty: 5\nsaw > prx: $9 qty: 10\n..."
+If the article is not in the catalog return "Nothing".
+
+s=
+s = """<prod><name>drill</name><prx>99</prx><qty>5</qty></prod>
+
+<prod><name>hammer</name><prx>10</prx><qty>50</qty></prod>
+
+<prod><name>screwdriver</name><prx>5</prx><qty>51</qty></prod>
+
+<prod><name>table saw</name><prx>1099.99</prx><qty>5</qty></prod>
+
+<prod><name>saw</name><prx>9</prx><qty>10</qty></prod>
+
+<prod><name>chair</name><prx>100</prx><qty>20</qty></prod>
+
+<prod><name>fan</name><prx>50</prx><qty>8</qty></prod>
+
+<prod><name>wire</name><prx>10.8</prx><qty>15</qty></prod>
+
+<prod><name>battery</name><prx>150</prx><qty>12</qty></prod>
+
+<prod><name>pallet</name><prx>10</prx><qty>50</qty></prod>
+
+<prod><name>wheel</name><prx>8.80</prx><qty>32</qty></prod>
+
+<prod><name>extractor</name><prx>105</prx><qty>17</qty></prod>
+
+<prod><name>bumper</name><prx>150</prx><qty>3</qty></prod>
+
+<prod><name>ladder</name><prx>112</prx><qty>12</qty></prod>
+
+<prod><name>hoist</name><prx>13.80</prx><qty>32</qty></prod>
+
+<prod><name>platform</name><prx>65</prx><qty>21</qty></prod>
+
+<prod><name>car wheel</name><prx>505</prx><qty>7</qty></prod>
+
+<prod><name>bicycle wheel</name><prx>150</prx><qty>11</qty></prod>
+
+<prod><name>big hammer</name><prx>18</prx><qty>12</qty></prod>
+
+<prod><name>saw for metal</name><prx>13.80</prx><qty>32</qty></prod>
+
+<prod><name>wood pallet</name><prx>65</prx><qty>21</qty></prod>
+
+<prod><name>circular fan</name><prx>80</prx><qty>8</qty></prod>
+
+<prod><name>exhaust fan</name><prx>62</prx><qty>8</qty></prod>
+
+<prod><name>window fan</name><prx>62</prx><qty>8</qty></prod>
+
+sample1:
+input: s,  'ladder'
+output: ladder > prx: $112 qty: 12
+
+sample2:
+input:  s , 'saw'
+output: 'table saw > prx: $1099.99 qty: 5\r\nsaw > prx: $9 qty: 10\r\nsaw for metal > prx: $13.80 qty: 32'
+'''
+
+def catalog(s, article):
+    
+    tags=s.split("\n")
+    att=[]
+    for st in tags:
+        if st!="":
+            att.append(st)
+#     print(att)
+    print(article)
+    res=[]
+    flag=0
+    for at in att:
+        if article in at:
+            flag=1
+#             print(at)
+            th=(at[at.index('<name>'):at.index('</name>')]).replace('<name>',"")
+            pr='prx: $'+(at[at.index('<prx>'):at.index('</prx>')]).replace('<prx>',"")
+            qt='qty: '+(at[at.index('<qty>'):at.index('</qty>')]).replace('<qty>',"")
+            res.append([th,pr,qt])
+    print(res)
+    if flag==0:
+        return "Nothing"
+    print(len(res))
+#     res_str="\r\n".join(res)
+    res_str=""
+    if len(res)==1:
+        res_str=f"{res[0][0]} > {res[0][1]} {res[0][2]}"
+        print(res_str)
+        return res_str
+    else:
+        for re in range(len(res)-1):
+            st=res[re]
+#             print(st)
+            res_str=res_str+f"{st[0]} > {st[1]} {st[2]}\r\n"
+#         print(res_str)
+    
+        st=res[len(res)-1]
+        res_str=res_str+f"{st[0]} > {st[1]} {st[2]}"
+        print(res_str)  
+        return res_str
+#     print(res_str)
+
+
+s= """<prod><name>drill</name><prx>99</prx><qty>5</qty></prod>
+
+<prod><name>hammer</name><prx>10</prx><qty>50</qty></prod>
+
+<prod><name>screwdriver</name><prx>5</prx><qty>51</qty></prod>
+
+<prod><name>table saw</name><prx>1099.99</prx><qty>5</qty></prod>
+
+<prod><name>saw</name><prx>9</prx><qty>10</qty></prod>
+
+<prod><name>chair</name><prx>100</prx><qty>20</qty></prod>
+
+<prod><name>fan</name><prx>50</prx><qty>8</qty></prod>
+
+<prod><name>wire</name><prx>10.8</prx><qty>15</qty></prod>
+
+<prod><name>battery</name><prx>150</prx><qty>12</qty></prod>
+
+<prod><name>pallet</name><prx>10</prx><qty>50</qty></prod>
+
+<prod><name>wheel</name><prx>8.80</prx><qty>32</qty></prod>
+
+<prod><name>extractor</name><prx>105</prx><qty>17</qty></prod>
+
+<prod><name>bumper</name><prx>150</prx><qty>3</qty></prod>
+
+<prod><name>ladder</name><prx>112</prx><qty>12</qty></prod>
+
+<prod><name>hoist</name><prx>13.80</prx><qty>32</qty></prod>
+
+<prod><name>platform</name><prx>65</prx><qty>21</qty></prod>
+
+<prod><name>car wheel</name><prx>505</prx><qty>7</qty></prod>
+
+<prod><name>bicycle wheel</name><prx>150</prx><qty>11</qty></prod>
+
+<prod><name>big hammer</name><prx>18</prx><qty>12</qty></prod>
+
+<prod><name>saw for metal</name><prx>13.80</prx><qty>32</qty></prod>
+
+<prod><name>wood pallet</name><prx>65</prx><qty>21</qty></prod>
+
+<prod><name>circular fan</name><prx>80</prx><qty>8</qty></prod>
+
+<prod><name>exhaust fan</name><prx>62</prx><qty>8</qty></prod>
+
+<prod><name>window fan</name><prx>62</prx><qty>8</qty></prod>"""
+catalog(s,'ladder')
